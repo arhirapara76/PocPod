fe79589610ceebfa91df0ec85c847226991823c1
Ajay <ajay@appringer.com>
2022-08-20 14:51:56 +0530

    - Project Setup

---
+++ PocPod/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PocPod
+//
+//  Created by iMac on 20/08/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PocPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PocPod
+//
+//  Created by iMac on 20/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
